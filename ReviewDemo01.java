10f94f9b22f824013f855bd5847067bbaa11c38c
hwchoi-inha <hwchoi@inha.edu>
2021-09-17 15:33:49 +0900

    ver 0.5

--- ReviewDemo01.java
+++ ReviewDemo01.java
@@ -7,9 +7,21 @@ public class ReviewDemo01 {
        int number = sc.nextInt();
        boolean isPrime = true;
 
-       for (int k=2; k<number; k++) {
-           if(number % k == 0) // 입력된 수를 k로 나누었을 때 나머지가 0인 경우
+//        for (int k=2; k<number; k++) {
+//            if(number % k == 0) { // 입력된 수를 k로 나누었을 때 나머지가 0인 경우
+//                isPrime = false;
+//                break;  // 첫 번째 약수가 발견되면 for문 탈출
+//            }
+//            System.out.println(k);
+//        }
+
+       int k=2;
+       while(k<number) {
+           if(number % k == 0) { // 입력된 수를 k로 나누었을 때 나머지가 0인 경우
                isPrime = false;
+               break;  // 첫 번째 약수가 발견되면 for문 탈출
+           }
+           k++;
        }
 
        if(isPrime)    // isPrime 변수 값이 true면
