3919df7b9845935653b4aab7cbcd214631789e39
hwchoi-inha <hwchoi@inha.edu>
2021-09-17 15:32:19 +0900

    ver 0.2

--- ReviewDemo01.java
+++ ReviewDemo01.java
@@ -6,15 +6,16 @@ public class ReviewDemo01 {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
         int cnt = 0;
-
-        for (int k=1; k<=number; k++) {
+ 
+        for (int k=2; k<number; k++) { // 반복 횟수 2회 감소
             if(number % k == 0)
                 cnt++;
         }
-
-        if(cnt == 2)
+ 
+        if(cnt == 0)    // 카운트가 0이 유지되면 true
             System.out.println(number + "은(는) 소수!");
         else
             System.out.println(number + "은(는) 소수가 아닙니다.");
     }
-}
+ }
+ 
